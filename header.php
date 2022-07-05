021a9400103cd060d33b9f2a9324c1383a6e79a6
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-07-05 16:51:15 -0500

    Navigation

---
+++ header.php
@@ -0,0 +1,13 @@
+	<header>     
+		<h1>Athletes Content Management System</h1>
+	</header>
+	<nav>
+		<ul>
+	    	<li><a href="index.php">Home</a></li>
+	    	<li><a href="post.php">Post Athletes</a></li>
+	    	<li><a href="categories.php">Categories</a></li> 
+	    	<li><a href="login.php">Login In</a></li>
+	    	<li><a href="logout.php">Login Out</a></li>
+	    	<li><a href="signup.php">Sign Up</a></li>        
+		</ul>
+	</nav>
