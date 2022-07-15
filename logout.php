788fd9e9874a24e9f33930ea46fef5e382d57cfd
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-07-15 08:41:56 -0500

    create logout system by destroy the login email session

--- logout.php
+++ logout.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Login Out</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
-</head>
-<body>
-	<?php include('header.php'); ?>
-	<main>
-		<form class="category">
-			<label for="categories">Categories:</label>
-			<select name="categories" id="categories">
-			  <option value="sport">Sport</option>
-			  <option value="country">country</option>
-			</select> 
-		</form>
-	</main>
-	<?php include('footer.php'); ?>
-</body>
-</html>
+<?php 
+session_start();
+
+session_destroy();
+
+header("location: index.php");
+exit;
+?>
