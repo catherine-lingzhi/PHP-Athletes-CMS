94c9df684b47fd72c58adc6d1738c9f3e5c56184
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-07-15 08:44:32 -0500

    change navigation login and signup to be disapeared and display login email when the user logined in

--- header.php
+++ header.php
@@ -5,9 +5,14 @@
 		<ul>
 	    	<li><a href="index.php">Home</a></li>
 	    	<li><a href="post.php">Post Athletes</a></li>
-	    	<li><a href="categories.php">Categories</a></li> 
-	    	<li><a href="login.php">Login In</a></li>
-	    	<li><a href="logout.php">Login Out</a></li>
-	    	<li><a href="signup.php">Sign Up</a></li>        
+	    	<li><a href="categories.php">Categories</a></li>
+	    	<?php if(!isset($_SESSION['email'])) :?>
+				<li><a href="login.php">Login</a></li>
+				<li><a href="signup.php">Sign Up</a></li>		
+			<?php endif ?>	    	
+	    	<li><a href="logout.php">Login Out</a></li>	    	 
+	    	<?php if(isset($_SESSION['email'])) :?>
+				<li>Hi, <?= $_SESSION['email'] ?>!</li>	
+			<?php endif ?>	 	    	     
 		</ul>
 	</nav>
