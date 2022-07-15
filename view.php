0eab8888119d8ce21e91bc02e2d8aaf19ce57431
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-07-15 08:39:29 -0500

    sumbit valid captcha code to add comments, print out wrong message when sumbit commit form

--- view.php
+++ view.php
@@ -1,25 +1,81 @@
 <!-------f----------
 
-    Assignment 3
+    CMS Project
     Name: Lingzhi Luo
-    Date: 2022-05-24
-    Description: The view page shows the detail of the posted title and all content.
+    Date: 2022-07-05 
 
 ------------------->
 <?php
+	session_start();
+
     require('connect.php');
+    require('vendor/autoload.php');   
 
-    if(isset($_GET['id'])){
-	    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-	  
-	    $query = "SELECT * FROM athletes WHERE id = :id LIMIT 1";
+	use Gregwar\Captcha\PhraseBuilder;
 
-	    $statement = $db->prepare($query);
-	    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+	if(isset($_GET['id'])){
+    	$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+  
+    	$query = "SELECT * FROM athletes WHERE id = :id LIMIT 1";
 
-	    $statement->execute();   
-	    $row = $statement->fetch();	
-	}    
+    	$statement = $db->prepare($query);
+    	$statement->bindValue(':id', $id, PDO::PARAM_INT);
+
+    	$statement->execute();   
+    	$row = $statement->fetch();	
+
+    	$c_query = "SELECT * FROM comments WHERE id = :id ORDER BY comment_time DESC";
+    	$c_statement = $db->prepare($c_query);
+    	$c_statement->bindValue(':id', $id);
+
+    	$c_statement->execute();
+	}
+
+	if(isset($_POST['add_comment'])){ 
+		 	
+		if(!empty($_POST['comment']) && !empty($_POST['captcha']) &&!empty($_POST['id'])){
+			$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+			$comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			$captcha = filter_input(INPUT_POST, 'captcha', FILTER_SANITIZE_FULL_SPECIAL_CHARS);				
+			
+			if(isset($_SESSION['email'])){
+				$user = substr($_SESSION['email'], 0, strpos($_SESSION['email'], '@'));
+			}
+
+			if(isset($_POST['user'])){
+				$user = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				if(empty($user)){
+					$error="Empty user.";
+				}
+			}	
+
+			if (isset($_SESSION['phrase']) && PhraseBuilder::comparePhrases($_SESSION['phrase'], $_POST['captcha'])) {
+				if(isset($user) && strlen($comment) > 0 && strlen($user) > 0 ){
+				
+					$query = "INSERT INTO comments (id, comment, user) VALUES (:id, :comment, :user)";
+					$statement = $db->prepare($query);
+
+			  		$statement->bindValue(':id', $id);
+			  		$statement->bindValue(':comment', $comment);
+			  		$statement->bindValue(':user', $user);
+			  		$statement ->execute();	
+			  		header("Location: view.php?id={$id}");					  	
+				}
+				else{
+					$error = "Empty input";
+				}
+			}	
+			else{
+				$error="Wrong captcha";
+
+			}
+			unset($_SESSION['phrase']);
+		}
+		else{
+			$error = "Empty input";;
+		}
+	}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,28 +88,51 @@
 	<?php include('header.php'); ?>
 
 	<main>
-    <?php if (isset($id)): ?>
+    <?php if (isset($_GET['id'])): ?>
         <form method="post">            
 			<div class="post">				
 				<h2><?= $row['name']?></h2>
 				<p>
 					<small>
-						<?= date_format(new datetime($row['edit_time']), "F j, Y, g:i a")?> 
+						<?= date_format(new datetime($row['create_time']), "F j, Y, g:i a")?> 
 						<a href="edit.php?id=<?= $row['id'] ?>">Edit</a>
 					</small>
 				</p>			
 				<div class="content">
 					<p><?= $row['content'] ?></p>
+				</div>
+				<div class="comments">
+					<h4>Comments:</h4><br>				
+					<?php while($c_row = $c_statement->fetch()): ?>			
+						<h5><?=$c_row['comment'] ?></h5>
+						<p>
+							<small>posted by <?=$c_row['user'] ?> - 
+							<?= date_format(new datetime($c_row['comment_time']), "F j, Y, g:i a")?>	
+							</small>
+						</p>
+					<?php endwhile ?>
+			
 				</div>			
 			</div>      
         </form>
+    <?php endif ?>
 
         <form method="post">
-        	<label for="comment">Comment:</label><br>	
-        	<textarea id="comment" name="comment" rows="10" cols="127"></textarea><br>
+        	<label for="comment">Make a Comment:</label><br>
+        	<input type="hidden" name="id" value="<?= $row['id'] ?>">
+        	<input type="hidden" name="comment_id" value="<?= $c_row['comment_id'] ?>">
+        	<?php if(!isset($_SESSION['email'])) :?>
+				<label for="user">User Name:</label>
+				<input type="text" name="user"/>	
+			<?php endif ?>	        		
+        	<textarea id="comment" name="comment" rows="10" cols="127"></textarea><br> 
+        	<img src="captcha.php"/><br>
+        	<input type="text" name="captcha" placeholder="type the code to varify" /><br><br>    	
         	<input type="submit" name="add_comment" value="Add Comment"/>
-        </form>	
-    <?php endif ?>
+        	<?php if(isset($error)) :?>
+				<p><?=$error ?></p>	
+			<?php endif ?>
+        </form>	    
 	</main>
 
 	<?php include('footer.php'); ?>
