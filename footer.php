370b042e77d1fc7b28e486875c9cea0f3c2c6385
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-07-24 20:50:02 -0500

    change footer sytle by using bootstrap grid system

--- footer.php
+++ footer.php
@@ -1,3 +1,33 @@
-	<footer>
-		<p>Copyright 2023 - No Rights Reserved</p>
-	</footer>
+    <footer class="page-footer font-small teal pt-4" style="background-color: #e3f2fd;">  
+        <div class="container-fluid text-left text-md-left">
+            <div class="row">  
+                <div class="col-md-6 mt-md-0 mt-3">   
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Quick Nav</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="post.php">Post Athletes</a></li>
+                        <li><a href="categories.php">Categories</a></li>
+                        <li><a href="user.php">Users Info</a></li> 
+                        <li><a href="login.php">Login</a></li> 
+                        <li><a href="signup.php">Sign Up</a></li>     
+                    </ul>
+                </div>     
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+         
+                <div class="col-md-3 mx-auto">        
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">CONTACT</h5>
+                    <ul class="list-unstyled">            
+                        <li>High Athletes Soprt Agent Co., Ltd</li>
+                        <li>ABC Street, Winnipeg City, Mantitoba, Canada</li>
+                        <li>Phone:+1-204-590-7595 <em>(24 hour support)</em></li>
+                        <li>Email:lingzhiluo8@outlook.com</li> 
+                    </ul>
+                </div>
+            </div>
+        </div>    
+          
+        <div class="footer-copyright text-center py-3"style="background-color: #AAA;">
+            <p>Copyright @ High Athletes Soprt Agent Co., Ltd. 2022 All Rights Reserved.</p>
+        </div>
+    </footer>
