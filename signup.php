297f01cfccb123d3805a668378900941221fc729
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-07-24 20:51:05 -0500

    change form style by using bootstrap form style

--- signup.php
+++ signup.php
@@ -8,47 +8,73 @@
 		else{
 			$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 			$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			$repeat_pwd = filter_input(INPUT_POST, 'repeat_pwd', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 			$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
+			$user_type = filter_input(INPUT_POST, 'user_type', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				echo $user_type;
 			if(!filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL)){
 				$error = "Invalid Email";
 			}
+			else if($password !== $repeat_pwd){
+				$error = "Pasword are not matched.";
+			}
 			else{
-				$query = "INSERT INTO users (email, password) VALUES (:email, :password)";
+				$query = "INSERT INTO users (email, password, user_type) VALUES (:email, :password, :user_type)";
 
 				$statement = $db->prepare($query);	
 				
 				$statement->bindValue(':email', $email);
 				$statement->bindValue(':password', $hashedPassword);
-	
+				$statement->bindValue(':user_type', $user_type);
 				$statement ->execute();
-				header("location: index.php");
+				header("location: login.php");
 			}
 		}
 	}
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sign Up</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 	<?php include('header.php'); ?>
 	<main>
+		<div class="container">
 		<h2>Sign Up</h2>
-		<form method="post">	
-			<label for="email">Email: </label>
-			<input type="text" name="email" placeholder="email"><br>		
-			<label for="password">Password: </label>
-			<input type="password" name="password" placeholder="password"><br>		
-			<input type="submit" name="signup" value="Sign Up"><br>
-			<?php if(isset($error)) :?>
-				<p><?=$error ?></p>
-			<?php endif ?>
-		</form>
+		<form method="post">  
+		  <div class="form-group">
+		    <label for="user_type">User Type: </label>
+		    <select class="form-control" name="user_type">		    
+		      <option value="user">User</option>
+		      <option value="admin">Admin</option>		 
+		    </select><br>
+		  </div> 
+		  <div class="form-group">
+		    <label for="email">Email address</label>
+		    <input type="email" class="form-control" name="email" placeholder="name@example.com"><br>
+		  </div> 
+		  <div class="form-group">
+		    <label for="password">Password: </label>
+			<input type="password" class="form-control" name="password"><br>
+		  </div>
+		  <div class="form-group">
+		    <label for="repeat_pwdrepeat_pwd">Repeat Password: </label>
+			<input type="password" name="repeat_pwd" class="form-control">
+		  </div>
+		  <?php if(isset($error)) :?>
+			<p><?=$error ?></p>
+		  <?php endif ?>
+		  <button type="submit" name="signup" class="btn btn-primary">Sign Up</button><br><br>
+		</form>		
+	</div>
 	</main>
 	<?php include('footer.php'); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
 </body>
 </html>
