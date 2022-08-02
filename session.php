be822da4368d3f197999cc87ed0d99c6c5d25af7
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-08-02 18:48:06 -0500

    create capthcha

---
+++ session.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+
+require ('vendor/autoload.php');   
+use Gregwar\Captcha\CaptchaBuilder;
+
+
+$captcha = new CaptchaBuilder;
+$_SESSION['phrase'] = $captcha->getPhrase();
+
+
+header('Content-Type: image/jpeg');
+
+
+$captcha
+    ->build()
+    ->output()
+;
