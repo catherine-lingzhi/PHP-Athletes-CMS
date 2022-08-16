c1552ce15e721842218e0adce6e7524b23734622
Lingzhi Luo <lingzhiluo8@outlook.com>
2022-08-16 08:17:04 -0500

    change connet information for the server

--- connect.php
+++ connect.php
@@ -7,10 +7,17 @@
 
 ------------------->
 <?php
+
+    // Connect http://highathletes.byethost16.com
+    // define('DB_DSN','mysql:host=sql213.byethost16.com;dbname=b16_32389503_serverside;charset=utf8');
+    // define('DB_USER','b16_32389503');
+    // define('DB_PASS','xmy0221'); 
+
+    //Connect the local host
     define('DB_DSN','mysql:host=localhost;dbname=serverside;charset=utf8');
     define('DB_USER','serveruser');
     define('DB_PASS','gorgonzola7!'); 
-
+    
     try{
     	$db = new PDO(DB_DSN, DB_USER, DB_PASS);
     }
